10336f50b494e05735eadb06d2d90e2c537d8e06
DiegoBM <1613216+DiegoBM@users.noreply.github.com>
2026-01-26 20:03:46 +0100

    Stablished connection to postgres database

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,28 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/DiegoBM/goWorkout/internal/api"
+	"github.com/DiegoBM/goWorkout/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// Our stores will go here
@@ -22,6 +30,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: api.NewWorkoutHandler(),
+		DB:             pgDB,
 	}
 
 	return app, nil
