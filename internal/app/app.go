8aba44280cfd9110d4b72eee22cccad8582bfe53
DiegoBM <1613216+DiegoBM@users.noreply.github.com>
2026-01-27 10:59:27 +0100

    Initial database migration

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/DiegoBM/goWorkout/internal/api"
 	"github.com/DiegoBM/goWorkout/internal/store"
+	"github.com/DiegoBM/goWorkout/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
